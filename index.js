95e2d88ca32e73f2d499be443f99ff0d51c5b8c4
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-08-06 23:12:50 +0600

    All api created

--- index.js
+++ index.js
@@ -14,26 +14,56 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8dkbmd5.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
- async function run() {
-    try{
-        await client.connect();
-        console.log('connected')
-        const productsCollections = client.db('jobTask').collection('products');
+async function run() {
+  try {
+    await client.connect();
+    console.log('connected')
+    const notesCollections = client.db('jobTask').collection('notes');
 
-        app.get('/products', async (req, res) =>{
-            const query = {};
-            const cursor = productsCollections.find(query);
-            const products = await cursor.toArray();
-            res.send(products)
-        })
+    //getting a note
+    app.get('/notes', async (req, res) => {
+      const query = {};
+      const cursor = notesCollections.find(query);
+      const notes = await cursor.toArray();
+      res.send(notes)
+    })
 
-    }
-    finally{
+    //posting a note
+    app.post('/notes', async (req, res) => {
+      const notes = req.body;
+      const result = await notesCollections.insertOne(notes);
+      res.send(result)
+    })
 
-    }
+
+    //Updating a note 
+
+    app.put('/notes/:id', async (req, res) => {
+      const id = req.params.id;
+      const notes = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: notes 
+      };
+      const result = await notesCollections.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+
+    //Deleting a data
+    app.delete('/notes/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await notesCollections.deleteOne(query);
+      res.send(result);
+    }) 
+
+  }
+  finally {
+
+  }
 
 }
 
@@ -43,9 +73,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello From todo app!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Todo app listening on port ${port}`)
 })
